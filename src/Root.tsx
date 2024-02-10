25331ed667a95148238e28d04683a9757e6f4629
Dylan Lee <leedylan0918@gmail.com>
2024-02-10 13:55:25 +0800

    Inject font open san-serif

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,7 +1,12 @@
+import "./styles/index.css";
 import React, { FunctionComponent } from "react";
 
 const Root: FunctionComponent = () => {
-  return <div className='App'>React App</div>;
+  return (
+    <React.Fragment>
+      <div className='App'>React App</div>
+    </React.Fragment>
+  );
 };
 
 export default Root;
